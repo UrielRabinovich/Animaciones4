d44d27a64b8d7337c9f7b05af730e20ca32a6c23
Uriel12 <136020019+UrielRabinovich@users.noreply.github.com>
2023-07-06 17:41:23 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,6 @@
+function girar (){
+    document.querySelector(".girar").classList.toggle("loop")
+}
+function generarArgentina(){
+    document.querySelector(".contenedor").innerHTML += '    <div class="cuadrado"></div>'
+}
